67d825e57b8ed450cf95b39c3318735d1fa90e71
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-03-16 15:54:41 -0700

    Implements script to convert XLS to CSV files successfully using Excel

---
+++ convert-xls-to-csv.ps1
@@ -0,0 +1,26 @@
+# Create a new Excel instance
+$excelApp = New-Object -ComObject Excel.Application
+
+# Turn off Excel alerts
+$excelApp.DisplayAlerts = $false
+
+# Get all the xls files in the folder
+Get-ChildItem -Path "C:\xlsFiles" -Filter "*.xls" | ForEach-Object {
+    # Opens the xls file
+    $workbook = $excelApp.Workbooks.Open($_.FullName)
+
+    # Defines the path for the CSV
+    $csvFilePath = $_.DirectoryName + "\" + ($_.BaseName + ".csv")
+    
+    # Save the XLS file as CSV
+    $workbook.SaveAs($csvFilePath, [Microsoft.Office.Interop.Excel.XlFileFormat]::xlCSV)
+
+    # Close the workbook
+    $workbook.Close()
+
+    # Deletes the original XLS file cuz who needs it
+    Remove-Item $_.FullName  
+}
+
+# Close Excel
+$excelApp.Quit()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# PowerShell Script to Convert XLS to CSV
+
+PowerShell Script to use Microsoft Excel (must be installed!) to batch convert a directory of XLS files into CSV files.
+
+DOES delete the original XLS files by default because I'm living that crazy life.
